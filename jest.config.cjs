92ebdca283e72d07432c7f54f2c3a4005373f579
Stefanig28 <stefanigtorres@gmail.com>
2025-11-06 13:05:06 -0500

    tests setup and workflows

---
+++ jest.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
