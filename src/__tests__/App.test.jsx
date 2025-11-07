9b37c4e63a6c9acf9f775f2636cbea23fa258111
Stefanig28 <stefanigtorres@gmail.com>
2025-11-07 16:42:14 -0500

    remove sample test

--- src/__tests__/App.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { App } from '../App';
-
-test('renders the navbar title', () => {
-  render(<App />);
-  expect(screen.getByText(/star wars album/i)).toBeInTheDocument();
-});
